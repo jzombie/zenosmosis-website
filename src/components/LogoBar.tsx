a16194fb21361bc9c7fc9f08f32047c3474b1aa1
Jeremy Harris <jeremy.harris@zenosmosis.com>
2025-11-10 15:14:51 -0600

    Scroll to top when logo is clicked

--- src/components/LogoBar.tsx
+++ src/components/LogoBar.tsx
@@ -2,9 +2,16 @@ import logoUrl from '../assets/zenOSmosis-logo.svg';
 import './LogoBar.css';
 
 export function LogoBar() {
+  const handleLogoClick = (event: React.MouseEvent<HTMLAnchorElement>) => {
+    event.preventDefault();
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
+
   return (
     <header className="logo-bar">
-      <img src={logoUrl} alt="zenOSmosis" className="logo" />
+      <a href="/" onClick={handleLogoClick}>
+        <img src={logoUrl} alt="zenOSmosis" className="logo" />
+      </a>
     </header>
   );
 }
